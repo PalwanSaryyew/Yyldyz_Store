cb0d801aa5895d1f2f6f5a7e9c3dc95fede8d6fd
palwan <palwansary1@gmail.com>
2025-04-26 16:33:08 +0500

    fix 5

--- app/src/app/(home)/page.tsx
+++ app/src/app/(home)/page.tsx
@@ -5,32 +5,38 @@ import { useRouter } from "next/navigation";
 import { paths, STORAGE_KEY } from "@/lib/settings";
 import { ProductType } from "../../../prisma/prismaSett";
 import Image from "next/image";
-import { webApp } from "@/lib/webApp";
 
 export default function HomePage() {
    const router = useRouter();
 
    useEffect(() => {
-      webApp();
-      let lastVisited = null;
-      try {
-         // get the last visited page from local storage
-         lastVisited = localStorage.getItem(STORAGE_KEY);
-      } catch (error) {
-         console.error(
-            "Failed to read last visited page from localStorage:",
-            error
-         );
-         localStorage.setItem(STORAGE_KEY, "/star");
-         router.refresh();
-      }
+      const prepair = async () => {
+         const WebApp = (await import("@twa-dev/sdk")).default;
+         await WebApp.ready();
+         let lastVisited = null;
+         try {
+            // get the last visited page from local storage
+            lastVisited = localStorage.getItem(STORAGE_KEY);
+         } catch (error) {
+            console.error(
+               "Failed to read last visited page from localStorage:",
+               error
+            );
+            localStorage.setItem(STORAGE_KEY, "/star");
+            router.refresh();
+         }
 
-      if (lastVisited && paths.includes(lastVisited.slice(1) as ProductType)) {
-         router.push(lastVisited);
-      } else {
-         localStorage.setItem(STORAGE_KEY, "/star");
-         router.refresh();
-      }
+         if (
+            lastVisited &&
+            paths.includes(lastVisited.slice(1) as ProductType)
+         ) {
+            router.replace(lastVisited);
+         } else {
+            localStorage.setItem(STORAGE_KEY, "/star");
+            router.refresh();
+         }
+      };
+      prepair()
    }, [router]); // This useeffect usually only works once because the router object will not change.
    return (
       <div className="text-white flex items-center justify-center h-[75vh] text-lg">
