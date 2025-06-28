7a34fb20733f1d230026ba110f9987f769ae6e81
palwan <palwansary1@gmail.com>
2025-06-28 19:12:32 +0500

    scrollable item menu

--- app/src/components/mains/ItemMenu.tsx
+++ app/src/components/mains/ItemMenu.tsx
@@ -5,14 +5,39 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ProductType } from "../../../prisma/prismaSett";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MdMore } from "react-icons/md";
 import { productTitle } from "bot/src/settings";
 
 const ItemMenu = () => {
    const currentPath = usePathname();
    const [active, setActive] = useState(false);
+   const [customPaths, setCustomPaths] = useState<ProductType[]>([]);
 
+   useEffect(() => {
+      const lasptPathsRaw = localStorage.getItem("lasptPaths");
+      let updatedCustomPaths: ProductType[] = [];
+      if (lasptPathsRaw) {
+         const lasptPaths: ProductType[] = lasptPathsRaw
+            ? JSON.parse(lasptPathsRaw)
+            : [];
+         lasptPaths.forEach((lasptPath) => {
+            if (!paths.includes(lasptPath)) {
+               lasptPaths.splice(lasptPaths.indexOf(lasptPath), 1);
+            } else {
+               updatedCustomPaths.push(lasptPath);
+            }
+         });
+         paths.forEach((path) => {
+            if (!lasptPaths.includes(path)) {
+               updatedCustomPaths.push(path);
+            }
+         });
+      } else {
+         updatedCustomPaths = [...paths];
+      }
+      setCustomPaths(updatedCustomPaths);
+   }, []);
    return (
       <>
          {active && (
@@ -27,17 +52,32 @@ const ItemMenu = () => {
                active
                   ? "backdrop-blur-[10px] bg-black/35"
                   : "backdrop-blur-[4px]",
-               " fixed right-0 top-1/2 -translate-y-1/2 z-10 rounded-s-lg border-2 border-r-0 border-mainColor max-h-[80vh]"
+               " fixed right-0 top-1/2 -translate-y-1/2 z-10 rounded-s-lg border-2 border-r-0 border-mainColor"
             )}
          >
-            <div className="overflow-y-auto flex flex-col divide-y divide-mainColor">
-               {paths.map((path) => (
+            <div className="overflow-y-auto max-h-[75vh] overflow-clip flex flex-col divide-y divide-mainColor">
+               {customPaths.map((path) => (
                   <Link key={path} href={"/" + path} prefetch>
                      <div
                         className={cn(
                            currentPath === "/" + path ? "bg-mainColor" : "",
                            "flex-1 p-1 px-2 flex items-center hover:bg-mainColor transition-colors duration-200 ease-in-out cursor-pointer"
                         )}
+                        onClick={() => {
+                           const lasptPathsRaw =
+                              localStorage.getItem("lasptPaths");
+                           const lasptPaths: ProductType[] = lasptPathsRaw
+                              ? JSON.parse(lasptPathsRaw)
+                              : [];
+                           if (lasptPaths.includes(path)) {
+                              lasptPaths.splice(lasptPaths.indexOf(path), 1);
+                           }
+                           lasptPaths.unshift(path as ProductType);
+                           localStorage.setItem(
+                              "lasptPaths",
+                              JSON.stringify(lasptPaths)
+                           );
+                        }}
                      >
                         <Image
                            src={`/jtns/${
