2e3c33e383c5da2c5b9fd6d84a3d0177407e567c
palwan <palwansary1@gmail.com>
2025-09-06 12:50:59 +0500

    doldur button opens tg link webapp closes

--- app/src/components/modals/modalItems/TopUpButton.tsx
+++ app/src/components/modals/modalItems/TopUpButton.tsx
@@ -3,21 +3,25 @@
 import { webApp } from "@/lib/webApp";
 
 const TopUpButton = () => {
-   async function handleTopUp() {
-      
-      (await webApp()).openTelegramLink("https://t.me/yyldyzbot?start=calladmin");
+    async function handleTopUp() {
+        // Telegram Web App nesnesini alalım
+        const tg = await webApp();
 
-      /* window.close(); 
-      window.open("https://t.me/yyldyzbot?start=calladmin"); */
-   }
-   return (
-      <button
-         className="w-full rounded-lg border p-2 text-gray-200 hover:bg-slate-200 hover:text-slate-800"
-         onClick={handleTopUp}
-      >
-         Doldur
-      </button>
-   );
+        // Önce Telegram linkini açalım
+        tg.openTelegramLink("https://t.me/yyldyzbot?start=calladmin");
+
+        // Ardından web uygulamasını kapatalım
+        tg.close();
+    }
+
+    return (
+        <button
+            className="w-full rounded-lg border p-2 text-gray-200 hover:bg-slate-200 hover:text-slate-800"
+            onClick={handleTopUp}
+        >
+            Doldur
+        </button>
+    );
 };
 
 export default TopUpButton;
