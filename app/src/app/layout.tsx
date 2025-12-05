aac12409942464bbbfbe089091020a3c6b886377
Emeki <palwansary1@gmail.com>
2025-12-05 12:55:44 +0500

    Snowfall

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -5,6 +5,7 @@ import TonProvider from "@/components/ton/TonProvidor";
 import Header from "@/components/mains/Header";
 import Footer from "@/components/mains/Footer";
 import ModalProvider from "@/components/modals/modalDinam/ModalProvider";
+import SnowfallClient from "@/components/mains/SnowfallClient";
 
 const geistSans = localFont({
    /*  src: "@/app/fonts/GeistVF.woff", */
@@ -33,6 +34,7 @@ export default async function RootLayout({
          <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
          >
+            <SnowfallClient />
             <TonProvider>
                <ModalProvider />
                <Header />
---
+++ app/src/components/mains/SnowfallClient.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import Snowfall from "react-snowfall";
+
+const SnowfallClient = () => {
+   return <Snowfall />;
+};
+
+export default SnowfallClient;
