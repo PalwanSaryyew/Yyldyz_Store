06ea96ac81a7bc6e764d7b08e706fca117125339
Emeki <palwansary1@gmail.com>
2025-12-25 20:55:16 +0500

    tmcell 13%

--- app/src/components/modals/BalansModal.tsx
+++ app/src/components/modals/BalansModal.tsx
@@ -6,9 +6,9 @@ const BalansModal = () => {
       <div className="bg-stone-900/50 backdrop-blur-sm w-10/12 max-h-screen p-4 border border-mainColor rounded-lg overflow-auto">
          <div className="text-white text-sm">Balans doldurmagyň usullary</div>
          <div className="text-white text-sm mt-2 flex flex-col divide-y ">
-            <TopupType fee={10}>TMCELL</TopupType>
-            <TopupType fee={2}>Bank</TopupType>
-            <TopupType fee={1}>Bankomat (kartdan karta)</TopupType>
+            <TopupType fee={13}>TMCELL</TopupType>
+            <TopupType fee={1.5}>Bank</TopupType>
+            <TopupType fee={0}>Bankomat (kartdan karta)</TopupType>
             <TopupType fee={0}>USDT</TopupType>
             <TopupType fee={0}>Nagt</TopupType>
          </div>
